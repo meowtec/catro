0683553aaa31b3d221069249300905f4d01713d7
Meowtec <bertonzh@gmail.com>
2016-02-18 18:43:23 +0800

    add https tests case, but ignore SSL unauthorized error.

--- src/__tests/proxy.test.ts
+++ src/__tests/proxy.test.ts
@@ -1,5 +1,6 @@
 'use strict'
 
+import * as path from 'path'
 import * as assert from 'assert'
 import * as Request from 'request'
 import Proxy from '../index'
@@ -12,153 +13,166 @@ const SSL_PORT = 10443
 
 const localhost = '127.0.0.1'
 
-Proxy.certPath = './cert'
+Proxy.certPath = path.resolve(__dirname, './cert')
 
 createHTTPServer(HTTP_PORT)
 createHTTPSServer(SSL_PORT)
 
 const proxy = new Proxy({
-  port: PROXY_PORT
+  port: PROXY_PORT,
+  https: true,
+  rejectUnauthorized: false
 })
 
 const request = Request.defaults({
+  strictSSL: false,
+  // ca: fs.readFileSync(Proxy.rootCAPath),
   proxy: 'http://' + localhost + ':' + PROXY_PORT
 })
 
-describe('#proxy', () => {
-  const httpServer = 'http://' + localhost + ':' + HTTP_PORT
+const RequestCallbackWrap = (callback: Function, done = (...args) => {}, async = false) => (
+  (error, response, data) => {
+    if (error) {
+      return done(error)
+    }
+    callback(error, response, data)
+    async || done()
+  }
+)
 
-  it('normal response', (done) => {
-    request(httpServer + '/0x00', (error, response, data) => {
-      assert.equal(data, 'hello world, protero!')
-      assert.equal(response.headers['content-type'], 'text/html; charset=utf-8')
-      assert.equal(response.headers['my-header'], '00')
-      done()
-    })
-  })
+function describeGenerate(https: boolean) {
+  const httpServer = https
+    ? 'https://' + localhost + ':' + SSL_PORT
+    : 'http://' + localhost + ':' + HTTP_PORT
 
-  it('normal request', (done) => {
-    request({
-      url: httpServer + '/0x01',
-      method: 'POST',
-      headers: {
-        'x-request': 'foo'
-      }
-    }, (error, response, data) => {
-      assert.equal(data, 'name-foo=value-foo&name-bar=value-bar')
-
-      assert.equal(response.headers['my-header'], 'foo')
-      done()
-    }).form({
-      'name-foo': 'value-foo',
-      'name-bar': 'value-bar'
-    })
-  })
-
-  it('visit http://proxy:port/', (done) => {
-    Request({
-      url: 'http://' + localhost + ':' + PROXY_PORT + '/',
-      method: 'GET'
-    }, (error, response, data) => {
-      assert.equal(data, 'hello meoproxy.')
-      done()
-    })
-  })
-
-
-  it('replace request headers', (done) => {
-    proxy.once('open', (requestHandler: RequestHandler) => {
-      requestHandler.replaceRequest = (request) => {
-        return Object.assign({}, request, {
-          headers: {
-            'x-request': 'request-replace-x'
-          }
-        })
-      }
+  return () => {
+    it('normal response', (done) => {
+      request(httpServer + '/0x00', RequestCallbackWrap((error, response, data) => {
+        assert.equal(data, 'hello world, protero!')
+        assert.equal(response.headers['content-type'], 'text/html; charset=utf-8')
+        assert.equal(response.headers['my-header'], '00')
+      }, done))
     })
 
-    request(httpServer + '/0x02', (error, response, data) => {
-      const json = JSON.parse(data)
-      assert.equal(json.headers['x-request'], 'request-replace-x')
-      done()
-    })
-  })
-
-  it('replace request body and method', (done) => {
-    proxy.once('open', (requestHandler: RequestHandler) => {
-      requestHandler.replaceRequest = (request) => {
-        return Object.assign({}, request, {
-          body: 'replaced.',
-          method: 'POST'
-        })
-      }
-    })
-
-    request(httpServer + '/0x02', (error, response, data) => {
-      const json = JSON.parse(data)
-      assert.equal(json.body, 'replaced.')
-      done()
-    })
-  })
-
-
-  it('replace response status and headers', (done) => {
-    proxy.once('open', (requestHandler: RequestHandler) => {
-      requestHandler.replaceResponse = (response) => {
-        return Object.assign({}, response, {
-          status: 404,
-          headers: {
-            'x-replace': 'y'
-          }
-        })
-      }
-    })
-
-    request(httpServer + '/0x00', (error, response, data) => {
-      assert.equal(response.statusCode, 404)
-      assert.equal(response.headers['x-replace'], 'y')
-      done()
-    })
-  })
-
-  it('replace response body', (done) => {
-    proxy.once('open', (requestHandler: RequestHandler) => {
-      requestHandler.replaceResponse = (response) => {
-        return Object.assign({}, response, {
-          body: '123456'
-        })
-      }
-    })
-
-    request(httpServer + '/0x00', (error, response, data) => {
-      assert.equal(data, '123456')
-      done()
-    })
-  })
-
-  it('handler events', (done) => {
-    const arr = [0, 0, 0]
-    proxy.once('open', (requestHandler: RequestHandler) => {
-      requestHandler.on('requestFinish', () => arr[0] = 1)
-      requestHandler.on('response', () => arr[1] = 1)
-      requestHandler.on('finish', () => {
-        arr[2] = 1
-
-        assert.deepEqual(arr, [1, 1, 1])
-        done()
+    it('normal request', (done) => {
+      request({
+        url: httpServer + '/0x01',
+        method: 'POST',
+        headers: {
+          'x-request': 'foo'
+        }
+      }, RequestCallbackWrap((error, response, data) => {
+        assert.equal(data, 'name-foo=value-foo&name-bar=value-bar')
+        assert.equal(response.headers['my-header'], 'foo')
+      }, done)).form({
+        'name-foo': 'value-foo',
+        'name-bar': 'value-bar'
       })
     })
 
-    request(httpServer + '/0x00', (error, response, data) => {})
-  })
-
-  it('handler url', (done) => {
-    proxy.once('open', (requestHandler: RequestHandler) => {
-      assert.equal(requestHandler.url, httpServer + '/0x00')
-      done()
+    it('visit http://proxy:port/', (done) => {
+      Request({
+        url: 'http://' + localhost + ':' + PROXY_PORT + '/',
+        method: 'GET'
+      }, RequestCallbackWrap((error, response, data) => {
+        assert.equal(data, 'hello meoproxy.')
+      }, done))
     })
 
-    request(httpServer + '/0x00', (error, response, data) => {})
-  })
 
-})
+    it('replace request headers', (done) => {
+      proxy.once('open', (requestHandler: RequestHandler) => {
+        requestHandler.replaceRequest = (request) => {
+          return Object.assign({}, request, {
+            headers: {
+              'x-request': 'request-replace-x'
+            }
+          })
+        }
+      })
+
+      request(httpServer + '/0x02', RequestCallbackWrap((error, response, data) => {
+        const json = JSON.parse(data)
+        assert.equal(json.headers['x-request'], 'request-replace-x')
+      }, done))
+    })
+
+    it('replace request body and method', (done) => {
+      proxy.once('open', (requestHandler: RequestHandler) => {
+        requestHandler.replaceRequest = (request) => {
+          return Object.assign({}, request, {
+            body: 'replaced.',
+            method: 'POST'
+          })
+        }
+      })
+
+      request(httpServer + '/0x02', RequestCallbackWrap((error, response, data) => {
+        const json = JSON.parse(data)
+        assert.equal(json.body, 'replaced.')
+      }, done))
+    })
+
+
+    it('replace response status and headers', (done) => {
+      proxy.once('open', (requestHandler: RequestHandler) => {
+        requestHandler.replaceResponse = (response) => {
+          return Object.assign({}, response, {
+            status: 404,
+            headers: {
+              'x-replace': 'y'
+            }
+          })
+        }
+      })
+
+      request(httpServer + '/0x00', RequestCallbackWrap((error, response, data) => {
+        assert.equal(response.statusCode, 404)
+        assert.equal(response.headers['x-replace'], 'y')
+      }, done))
+    })
+
+    it('replace response body', (done) => {
+      proxy.once('open', (requestHandler: RequestHandler) => {
+        requestHandler.replaceResponse = (response) => {
+          return Object.assign({}, response, {
+            body: '123456'
+          })
+        }
+      })
+
+      request(httpServer + '/0x00', RequestCallbackWrap((error, response, data) => {
+        assert.equal(data, '123456')
+      }, done))
+    })
+
+    it('handler events', (done) => {
+      const arr = [0, 0, 0]
+      proxy.once('open', (requestHandler: RequestHandler) => {
+        requestHandler.on('requestFinish', () => arr[0] = 1)
+        requestHandler.on('response', () => arr[1] = 1)
+        requestHandler.on('finish', () => {
+          arr[2] = 1
+
+          assert.deepEqual(arr, [1, 1, 1])
+          done()
+        })
+      })
+
+      request(httpServer + '/0x00', RequestCallbackWrap((error, response, data) => {}))
+    })
+
+    it('handler url', (done) => {
+      proxy.once('open', (requestHandler: RequestHandler) => {
+        assert.equal(requestHandler.url, httpServer + '/0x00')
+        done()
+      })
+
+      request(httpServer + '/0x00', RequestCallbackWrap((error, response, data) => {}))
+    })
+
+  }
+}
+
+describe('#proxy:HTTP', describeGenerate(false))
+describe('#proxy:HTTPS', describeGenerate(true))
